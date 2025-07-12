d051227b2bc31c8253194d265c3d250df8fc0b1b
Tarun Sharma <tarunshama2002@gmail.com>
2025-07-12 22:42:53 +0530

    third commit

--- 09_AsynJS_and_callbackFunction.js
+++ 09_AsynJS_and_callbackFunction.js
@@ -4,7 +4,7 @@ console.log('Tarun')
         
 setTimeout(function(){
     console.log('Sharma')
-    }, 0) // this is a Asynchoronous function . here it have the run time of 0 sec . means take 0 time to execute
+    }, 0) // setTimeout is a Asynchoronous function . here it have the run time of 0 sec . means take 0 time to execute
 
 // but its result will appears in the last as it firsts moves to the Side stack that is a queue and meanwhile all
 // the synchoronous functions gets exexuted than it moves in main call stack for execution.
@@ -23,4 +23,11 @@ console.log('You are learning Web Dev')
 
 
 // the function written inside the setTImeOut is a callback function it will only get executes after the completion of the 
-// time give but after all the syn is executed.
+// time given but after all the syn instructions are executed.
+
+// A callback function is a function passed into another function as an argument , which is then involved inside the outer
+// function to complete an action.
+
+// Call Back Hell ==> Nested callbacks stacked below one another forming a pyramid structure. (pyramid of doom)
+// This type of programming becomes difficult to understand and manage.
+
