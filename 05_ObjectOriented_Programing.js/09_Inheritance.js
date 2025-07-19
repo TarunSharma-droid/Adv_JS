6fc456bc25a0a7f5409f11609547f8dff7bb5e92
Tarun Sharma <tarunshama2002@gmail.com>
2025-07-19 17:02:07 +0530

    commit

---
+++ 05_ObjectOriented_Programing.js/09_Inheritance.js
@@ -0,0 +1,34 @@
+
+const Human = {
+
+    age : "more then 25",
+    location : "Ajmer"
+}
+
+const Teacher = {
+
+    makeVideo : true
+}
+
+const TeacherAssistance = {
+    
+    isAvailable : false
+}
+
+const DoubtSupport = { 
+
+    makeAssignment : 'JS Assignments',
+    fulltime : true,
+
+    __proto__ : TeacherAssistance 
+}
+ // we know that every object has its own prototype => __proto__ 
+ // this prototype is used to take referene of Constructor function , any other function (make connection)
+ // here this object prototype is pointing (refering) to a function "TeacherAssistance" means accessing
+ // the properties of TeacherAssistance.
+
+
+ TeacherAssistance.__proto__ = Human // another syntax to access properties of another function.
+
+ Object.setPrototypeOf(Teacher , Human) // another syntax to access properties of another function.
+ // Teacher have access to the property of Human.
