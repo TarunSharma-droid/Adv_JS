f73d1592946389b12758d4d9744421deef40c534
Tarun Sharma <tarunshama2002@gmail.com>
2025-07-20 17:03:28 +0530

    commit

--- 05_ObjectOriented_Programing.js/15_Explicite_Calling.js
+++ 05_ObjectOriented_Programing.js/15_Explicite_Calling.js
@@ -6,7 +6,8 @@ function SetUserName (getname){
 }
 
 function createUser (username , email , password){
-        
+       
+    SetUserName.call(this, username); 
     this.mail_ofUser = email
     this.password_ofUser = password
 
@@ -16,6 +17,18 @@ function createUser (username , email , password){
 const u1 = new createUser("Tarun" ,"tarunshama@gmail.com" , 2002)
 
 console.log(u1)
+
+/*
+When we normally call new SetUserName(...), the this inside SetUserName points to a 
+new object created by it.
+
+But here, we don’t want to create a separate object — we want SetUserName to set properties
+on the object being created by createUser.
+
+So instead of letting SetUserName create its own this, we pass the this from createUser
+into SetUserName using .call(this, ...).
+
+That way, both constructors work on the same object, and all properties stay together.  */
 
 /* 
 Object.setPrototypeOf(createUser, SetUserName)
